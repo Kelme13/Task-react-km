3a282ca992890a35eb76f2020a7dbcfd4d75ab10
Kelvin Melgar <kelvinmelgar777@gmail.com>
2024-03-02 11:42:19 -0600

    First commit

---
+++ src/tasks.js
@@ -0,0 +1,23 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "Mi primer tarea",
+    description: "Mi primer tareas",
+  },
+  {
+    id: 1,
+    title: "Mi segunda tarea",
+    description: "Mi segunda tareas",
+  },
+  {
+    id: 2,
+    title: "Mi tercer tarea",
+    description: "Mi tercer tareas",
+  },
+  {
+    id: 3,
+    title: "Mi cuarta tarea",
+    description: "Mi cuarta tareas",
+  },
+];
+ 
